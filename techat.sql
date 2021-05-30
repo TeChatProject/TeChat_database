fb779b45e37a8fcbe9a61154ab9f9520a81d76b7
Furkan Sayan <58613119+Vicoth@users.noreply.github.com>
2021-05-30 19:27:34 +0300

    Update techat.sql

--- techat.sql
+++ techat.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2021 at 11:20 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: May 27, 2021 at 03:03 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `event` (
   `event_id` int(10) NOT NULL,
-  `event_loc_id` int(7) NOT NULL,
+  `loc_id` int(7) NOT NULL,
   `event_date` date NOT NULL,
   `event_type` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -44,9 +44,9 @@ CREATE TABLE `location` (
   `loc_id` int(7) NOT NULL,
   `loc_name` varchar(45) NOT NULL,
   `loc_address` varchar(120) NOT NULL,
-  `loc_event_id` int(11) NOT NULL,
-  `loc_post_id` int(11) NOT NULL,
-  `loc_place_id` int(11) NOT NULL
+  `event_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `place_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -56,7 +56,7 @@ CREATE TABLE `location` (
 --
 
 CREATE TABLE `login_info` (
-  `login_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `log_username` varchar(30) NOT NULL,
   `log_psw` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -69,7 +69,7 @@ CREATE TABLE `login_info` (
 
 CREATE TABLE `place` (
   `place_id` int(10) NOT NULL,
-  `place_loc_id` int(7) NOT NULL,
+  `loc_id` int(7) NOT NULL,
   `place_score` int(2) NOT NULL,
   `place_comment` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -82,7 +82,7 @@ CREATE TABLE `place` (
 
 CREATE TABLE `post` (
   `post_id` int(14) NOT NULL,
-  `post_user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `post_type` int(2) NOT NULL,
   `post_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -117,31 +117,37 @@ INSERT INTO `user` (`user_id`, `email`, `user_namesur`, `degree`, `grade`, `user
 -- Indexes for table `event`
 --
 ALTER TABLE `event`
-  ADD PRIMARY KEY (`event_id`);
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `loc_id` (`loc_id`);
 
 --
 -- Indexes for table `location`
 --
 ALTER TABLE `location`
-  ADD PRIMARY KEY (`loc_id`);
+  ADD PRIMARY KEY (`loc_id`),
+  ADD KEY `event_id` (`event_id`),
+  ADD KEY `post_id` (`post_id`) USING BTREE,
+  ADD KEY `place_id` (`place_id`) USING BTREE;
 
 --
 -- Indexes for table `login_info`
 --
 ALTER TABLE `login_info`
-  ADD PRIMARY KEY (`login_id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- Indexes for table `place`
 --
 ALTER TABLE `place`
-  ADD PRIMARY KEY (`place_id`);
+  ADD PRIMARY KEY (`place_id`),
+  ADD KEY `loc_id` (`loc_id`);
 
 --
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
-  ADD PRIMARY KEY (`post_id`);
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `user_id` (`user_id`) USING BTREE;
 
 --
 -- Indexes for table `user`
@@ -169,7 +175,7 @@ ALTER TABLE `location`
 -- AUTO_INCREMENT for table `login_info`
 --
 ALTER TABLE `login_info`
-  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `place`
@@ -188,10 +194,36 @@ ALTER TABLE `post`
 --
 
 --
+-- Constraints for table `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `location` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `location`
+--
+ALTER TABLE `location`
+  ADD CONSTRAINT `location_ibfk_1` FOREIGN KEY (`loc_id`) REFERENCES `event` (`loc_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `login_info`
 --
 ALTER TABLE `login_info`
-  ADD CONSTRAINT `login_info_ibfk_1` FOREIGN KEY (`login_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `login_info_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `place`
+--
+ALTER TABLE `place`
+  ADD CONSTRAINT `place_ibfk_1` FOREIGN KEY (`loc_id`) REFERENCES `location` (`loc_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `place_ibfk_2` FOREIGN KEY (`place_id`) REFERENCES `location` (`place_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `location` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
